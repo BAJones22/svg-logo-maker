f8c1e8e533a8acd6eaee41651d271fba01d2453b
Brooke Adlesperger-Jones <badles@live.com>
2023-05-30 18:01:39 -0500

    added tests

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
-// Packages needed for this application
 const fs = require("fs");
 const inquirer = require("inquirer");
-const { createLogo } = require("./lib/createLogo");
-const { questions } = require("./lib/questions");
+const createShape = require("./lib/createShape");
+const questions = require("./lib/questions");
 
-// Function to ask questions
-function askQuestions() {
-    inquirer.prompt(questions).then(function (answers) {
-      fs.appendFile(
-        "logo.svg",
-        createLogo(answers),
-        "utf-8",
-        function (err) {
-          console.error(err);
-        }
-      );
-    });
-  }
-  
-  // Function to initialize app
-  function init() {
-    askQuestions();
-  }
-  
-  // // Function call to initialize app
-  init();
-  
+// Function to create a new SVG file titled "logo.svg" using inquirer response
+function saveLogoToFile(logoData) {
+  const svg = createShape(logoData);
+  fs.writeFile("logo.svg", svg, "utf-8", function (err) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.log("Logo.svg created successfully.");
+    }
+  });
+}
+
+// Function to ask questions and create logo
+function createLogo() {
+  inquirer.prompt(questions).then(function (answers) {
+    saveLogoToFile(answers);
+  });
+}
+
+// Function to initialize the app
+function init() {
+  createLogo();
+}
+
+// Function call to initialize the app
+init();
